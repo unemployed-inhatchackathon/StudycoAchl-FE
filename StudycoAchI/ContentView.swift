8e71cc05088434d577e80936218f401ce5157fa5
JaeSunEo <wotjs020708@gmail.com>
2025-08-06 15:56:51 +0900

    Initial Commit

---
+++ StudycoAchI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudycoAchI
+//
+//  Created by 어재선 on 8/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudycoAchI/StudycoAchIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudycoAchIApp.swift
+//  StudycoAchI
+//
+//  Created by 어재선 on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StudycoAchIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
