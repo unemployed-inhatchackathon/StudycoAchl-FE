245bcead159b127bd1f4912b2072e2cb4cd6d3d4
JaeSunEo <wotjs020708@gmail.com>
2025-08-22 15:00:08 +0900

    feat: Logging 플로그인 추가

---
+++ StudycoAchI/Core/Extensions/OS+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  OS+Extension.swift
+//  StudycoAchI
+//
+//  Created by 어재선 on 8/22/25.
+//
+
+import Foundation
+import OSLog
+
+extension OSLog {
+    static let subsystem = Bundle.main.bundleIdentifier!
+    static let network = OSLog(subsystem: subsystem, category: "Network")
+    static let debug = OSLog(subsystem: subsystem, category: "Debug")
+    static let info = OSLog(subsystem: subsystem, category: "Info")
+    static let error = OSLog(subsystem: subsystem, category: "Error")
+}
+
